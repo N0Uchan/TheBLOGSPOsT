b2cd8220248ab8e5fa53f918576dfc51c3856cc5
N0Uchan <142255222+N0Uchan@users.noreply.github.com>
2024-02-29 11:53:33 +0530

    replace browser routing with hashrouting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter , RouterProvider } from 'react-router-dom' ;
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SignIn from './pages/SignIn';
 import Welcome from './pages/Welcome';
 import RootLayout from './pages/Root';
-import ErrorPage, {ErrorPage2} from './pages/Error';
+import ErrorPage from './pages/Error';
 import UserHome from './pages/UserHome';
 import Home from './pages/Home';
 import Posts from './pages/Posts';
@@ -11,34 +11,70 @@ import NewPost from './pages/NewPost'
 import UserPosts from './pages/UserPosts';
 import PostById from './pages/PostById';
 
-const router = createBrowserRouter([
-  {
-    path: '/TheBLOGSPOsT',
-    element : <RootLayout />,
-    errorElement: <ErrorPage />,
-    children : [
-      { index:true , element: <Welcome /> },
-      { path: 'signIn', element: <SignIn /> },
-    ]
-  } ,
-  { 
-    path : 'TheBLOGSPOsT/home',
-    element : <UserHome />,
-    errorElement  : <ErrorPage2 />,
-    children : [
-      { index: true, element: <Home /> },
-      { path: 'posts', element: <Posts /> },
-      { path: 'posts/:id', element: <PostById />},
-      { path: 'newPost', element: <NewPost /> },
-      { path: 'posted', element: <UserPosts />} 
-    ]
-  }
-])
-
 export default function App() {
-
-  return < RouterProvider router={router} />
-
+  return (
+    <Router>
+      <Routes>
+        <Route path="/TheBLOGSPOsT" element={<RootLayout />}>
+          <Route index element={<Welcome />} />
+          <Route path="signIn" element={<SignIn />} />
+          <Route path="home" element={<UserHome />}>
+            <Route index element={<Home />} />
+            <Route path="posts" element={<Posts />} />
+            <Route path="posts/:id" element={<PostById />} />
+            <Route path="newPost" element={<NewPost />} />
+            <Route path="posted" element={<UserPosts />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
 }
+
+
+
+// import { createBrowserRouter , RouterProvider } from 'react-router-dom' ;
+// import './App.css';
+// import SignIn from './pages/SignIn';
+// import Welcome from './pages/Welcome';
+// import RootLayout from './pages/Root';
+// import ErrorPage, {ErrorPage2} from './pages/Error';
+// import UserHome from './pages/UserHome';
+// import Home from './pages/Home';
+// import Posts from './pages/Posts';
+// import NewPost from './pages/NewPost'
+// import UserPosts from './pages/UserPosts';
+// import PostById from './pages/PostById';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/TheBLOGSPOsT',
+//     element : <RootLayout />,
+//     errorElement: <ErrorPage />,
+//     children : [
+//       { index:true , element: <Welcome /> },
+//       { path: 'signIn', element: <SignIn /> },
+//     ]
+//   } ,
+//   { 
+//     path : 'TheBLOGSPOsT/home',
+//     element : <UserHome />,
+//     errorElement  : <ErrorPage2 />,
+//     children : [
+//       { index: true, element: <Home /> },
+//       { path: 'posts', element: <Posts /> },
+//       { path: 'posts/:id', element: <PostById />},
+//       { path: 'newPost', element: <NewPost /> },
+//       { path: 'posted', element: <UserPosts />} 
+//     ]
+//   }
+// ])
+
+// export default function App() {
+
+//   return < RouterProvider router={router} />
+
+// }
 
 
