990038159843469e6b8e98cc5434089ca212ad92
N0Uchan <142255222+N0Uchan@users.noreply.github.com>
2024-03-04 23:17:07 +0530

    color theme 50% progress

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,16 +3,16 @@ export default function Post({ author, title, content , date , img}) {
 
 
     return( 
-        <main className="" id="PostPage" >
+        <main className="" >
             <section id="postx" >
                 
                 <header className='details' >
-                    <h4 className='detailsH' >@ {author}</h4>
-                    <h6 className='detailsH' >{date}</h6>
+                    <h4 className='detailsH innTextColor' >@ {author}</h4>
+                    <h6 className='detailsH innTextColor' >{date}</h6>
                 </header>
-                <h2 className='postTitle' >{title}</h2>
+                <h2 className='postTitle innTextColor' >{title}</h2>
                 <img className='postImg' src={img} alt="Posted Image" />
-                <p className='detailsP' >{content}</p>
+                <p className='detailsP innTextColor' >{content}</p>
             </section>
         </main>
     )
