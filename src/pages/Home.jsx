793f6f8ac4acb1acf8303dcdadb687f23a657bc6
N0Uchan <142255222+N0Uchan@users.noreply.github.com>
2024-03-04 15:40:41 +0530

    remove note : forgot to remove b4 ;d

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,9 +38,7 @@ export default function Home() {
                   <div className="headerItems" >
                     <h1 id='PostsHeader'  >Latest Updates</h1>
                     <button className="refreshBtn" onClick={refreshPosts1} >RefIcon</button>
-                  </div>
-                  <h2>ADD UID CHECK FOR CREATE POST</h2>
-                
+                  </div>               
                     {isFetching && <p>Loading Latest Updates for You..</p>}
                     {!isFetching && posts1.length === 0 && <p>No Posts Available</p>}
                     {!isFetching && posts1.length > 0 && (<>
