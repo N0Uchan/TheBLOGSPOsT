cbca0b7d405611aa48083bf31ffb1c4a95f74580
N0Uchan <142255222+N0Uchan@users.noreply.github.com>
2024-05-05 10:41:43 +0530

    add error msg and interaction while creating new post

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -14,10 +14,15 @@ export default function NewPost() {
     const author = userDetails.author ; // given name
     const email = userDetails.email;
     const uid = userDetails.uid;
+
+    const errMsg=useRef();
+    const errorMsg = <p id="errorMsg" ref={errMsg} className='' ></p>
+
     function submitNewPost(){
-        const title = titleRef.current.value;
+        const title = titleRef.current.value.trim();
+        const content = contentRef.current.value.trim();
         // const img = imgRef.current.value;    IMAGE LATER DONT FORGETTTT
-        const content = contentRef.current.value;
+        
         if (title.length > 3 && content.length > 0 ){
             createNewPost(email,title,content,author,uid).then((res)=>{
                 // console.log(res);
@@ -26,10 +31,22 @@ export default function NewPost() {
                 // console.log(err);
             })
         }else{
-            console.log("Invalid Input");
+            if(title==='' ||title.length<4 ){
+                errMsg.current.textContent= 'Please enter a Title, maximum 20 characters.'
+                errMsg.current.className="vibrate";
+                titleRef.current.className="inputBox invalidField";
+                setTimeout(()=>{errMsg.current.className=""},200)
+                setTimeout(()=>{titleRef.current.className="inputBox"},200)
+              }
+              else if(content==='' || content.length>200 ){
+                errMsg.current.textContent= 'Please enter a Description , less than 200 characters.'
+                errMsg.current.className="vibrate";
+                contentRef.current.className="textAreaBox invalidField";
+                setTimeout(()=>{errMsg.current.className=""},200)
+                setTimeout(()=>{contentRef.current.className="textAreaBox"},200)
+              }        
         }
     }
-
 
     return ( 
         <main className="pages newPostPage" >
@@ -39,26 +56,26 @@ export default function NewPost() {
                 
                 <div className="formItmGrp" >
                     <label htmlFor="title" className="newPostLabels">Title</label>
-                    <input type="text" className="inputBox" 
+                    <input id="newTitle" type="text" className="inputBox" 
                            ref={titleRef}      />
                 </div>
 
                 <div className="formItmGrp">
                     <label htmlFor="image" className="newPostLabels">Image</label>
-                    <input type="file" className="inputBox" accept="image/*"
+                    <input type="file" id="newImg" className="inputBox" accept="image/*"
                             ref={imgRef} />
                 </div>
 
                 <div className="formItmGrp" >
                     <label htmlFor="content" className="newPostLabels" >Content</label>
-                    <textarea id="" rows="5" className="textAreaBox" ref={contentRef} ></textarea>
+                    <textarea rows="5" id="newContent" className="textAreaBox" ref={contentRef} ></textarea>
                 </div>
 
                 <div id="formSumbitBtnGrp" >
-                    <button type="button" onClick={()=>{submitNewPost()}}  >Create Post</button>
+                    <button type="button" id="createBtn" onClick={()=>{submitNewPost()}}  >Create Post</button>
                     <Link to=".."  > Cancel </Link>
                 </div>
-                
+                {errorMsg}
             </form>
             
             </section>
