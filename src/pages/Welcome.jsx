56cb4c84085626aa25cfdd913a453dc6818d93c6
N0Uchan <142255222+N0Uchan@users.noreply.github.com>
2024-06-13 12:42:09 +0530

    welcome page text

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 export default function HomeDef() {
   return (
     <main className='pages'>
-      <h1>Welcome</h1>
-      <Link to='/signIn' > Sign In </Link>
+      <h1>Welcome To The BLOGSPOST .</h1>
+      <h2>Please Sign In To continue.</h2>
+      <h2><Link to='/signIn' > Sign In </Link></h2>
+      
     </main>
   );
 }
